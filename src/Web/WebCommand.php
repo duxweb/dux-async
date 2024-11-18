d1c830bc6d5897603cd0571d78b280761739b745
mr.l <admin@duxphp.com>
2024-11-18 20:29:19 +0800

    修正psr7桥接

--- src/Web/WebCommand.php
+++ src/Web/WebCommand.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 
 namespace Core\Web;
 
-use Chubbyphp\SwooleRequestHandler\PsrRequestFactory;
 use Chubbyphp\SwooleRequestHandler\SwooleResponseEmitter;
 use Core\App;
 use Core\Coroutine\ContextManage;
